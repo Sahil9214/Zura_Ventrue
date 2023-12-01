a2e864a3288c5c1cf410a817cd05ad40c2293026
Utkarsh <utkarshsinghal369@gmail.com>
2023-12-01 09:50:01 +0530

    frontend

--- Frontend/zura-venture/src/App.js
+++ Frontend/zura-venture/src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
-import AllRouter from './Router/AllRouter';
-import Footer from './Components/Footer';
+import AllRoutes from './Routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <AllRouter/>
-     <Footer/>
+      <AllRoutes />
     </div>
   );
 }
--- Frontend/zura-venture/src/index.js
+++ Frontend/zura-venture/src/index.js
@@ -2,23 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 import { ChakraProvider } from "@chakra-ui/react";
-import {store} from './Store/store.js'
+import { Provider } from 'react-redux';
+import {store } from './Redux/store';
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <ChakraProvider>
+    <BrowserRouter>
     <Provider store={store}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Provider>
-  </BrowserRouter>
+    <App />
+  </Provider>,
+    </BrowserRouter>
+  </ChakraProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
